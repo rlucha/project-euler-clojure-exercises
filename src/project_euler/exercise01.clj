14c129c4dd83715444823b46c12b71174ce71298
Roberto Lucha <rlucha@gmail.com>
2016-03-13 20:02:14 +0100

    Added description of 1st problem

--- src/project_euler/exercise01.clj
+++ src/project_euler/exercise01.clj
@@ -1,5 +1,11 @@
 (ns project-euler.exercise01)
 
+; If we list all the natural numbers below 10 that are multiples of 3 or 5,
+; we get 3, 5, 6 and 9. The sum of these multiples is 23.
+; Find the sum of all the multiples of 3 or 5 below 1000.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
 (def numbers (range 1000))
 
 (defn isMultiple3or5 [number]
@@ -7,5 +13,5 @@
     number
     0))
 
-; solution is 233168
+; Solution is 233168
 (reduce + (map isMultiple3or5 numbers))
