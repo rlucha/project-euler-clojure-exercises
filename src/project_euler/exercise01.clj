da8219ca5c12a80f451b1038208ff4d819606e96
Roberto Lucha <rlucha@gmail.com>
2016-03-13 20:00:44 +0100

    1st exercise finished

--- src/project_euler/exercise01.clj
+++ src/project_euler/exercise01.clj
@@ -1,10 +1,11 @@
 (ns project-euler.exercise01)
 
-(def numbers (range 10))
+(def numbers (range 1000))
 
 (defn isMultiple3or5 [number]
   (if (or (= (mod number 5) 0) (= (mod number 3) 0))
     number
     0))
 
+; solution is 233168
 (reduce + (map isMultiple3or5 numbers))
