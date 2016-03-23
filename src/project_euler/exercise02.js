f6a7834ee289272702d725c675b244ba2813831a
Roberto Lucha <rlucha@gmail.com>
2016-03-23 04:31:47 +0100

    exercise2 cleanup

--- src/project_euler/exercise02.js
+++ src/project_euler/exercise02.js
@@ -1,24 +1,3 @@
-
-
-function makeFib(a, b, length) {
-  let result = [a,b];
-  while (true) {
-    c = a + b;
-    if(result.length > length) break
-    result.push(c)
-    a = b;
-    b = c;
-  }
-  return result;
-}
-
-function* naturalNumbers() {
-  let i = 0;
-  while(true) {
-    yield i++;
-  }
-}
-
 function* fib(a, b) {
   let c = 0
   yield a
@@ -54,5 +33,3 @@ function sum(a,b) { return a + b }
 function isEven(n) { return n % 2 === 0}
 
 const solution = takeWhile(n => n < 4000000, fib(1,2)).filter(isEven).reduce(sum)
-
-4613732
